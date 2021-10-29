0394897396bba616e596e82fb3a0e70bc6a157f5
Michael Stafford <michaelstafford808@gmail.com>
2021-10-28 18:54:08 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 // Code your solutions in this file
-const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+
+// for (let age = 30; age < 40; age++) {
+//   console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+
+const names = ['mike', 'joe', 'kev'];
+
 
 function writeCards(names, event) {
-  let birthday =[];
+  let birthday = []; // i don't understand why birthday is assigned a value of an empty object 
   event = "surprise";
-  for (let i= 0; i < names.length; i++) {
-  birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+  for (let i = 0; i < names.length; i++) {
+    birthday[i] = ('Thank you, ' + names[i] + ', for the wonderful ' + event +' gift!')
   }
   return birthday;
 }
+// ask for help in js prework support on how to use interpolation
 
-
-let number = 10;
-function countDown (number) {
-  while (number >= 0) {
-    console.log(number--);
-  }
+function countDown(number) {
+let countDown = 10;
+while (number >= 0) {
+  console.log(number--);
+}
 }
